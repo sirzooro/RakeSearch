5d927ce3c4c0be0d7d9085a6b95914295138a481
Nevecie <nevecie@yandex.ru>
2017-10-22 21:43:51 +0300

    Comments translated - Generator.h

--- RakeDiagSearch/RakeDiagSearch/Generator.h
+++ RakeDiagSearch/RakeDiagSearch/Generator.h
@@ -1,4 +1,4 @@
-// Генератор Диагональных латинских квадратов
+// DLS generator
 
 # if !defined Generator_h
 # define Generator_h
@@ -16,62 +16,62 @@ class MovePairSearch;
 class Generator
 {
 public:
-	Generator();					// Конструктор по умолчанию
-	Generator(Generator& source);	// Конструктор копировния
-	void Start();					// Запуск генерации квадратов
-	void Reset();					// Сброс всех значений внутренних структур
-	void SetFileNames(string start, string result, string checkpoint, string temp);	// Заданием имен файлов параметров и контрольной точки
-	void Initialize(string start, string result, string checkpoint, string temp);	// Инициализация поиска
-	void Subscribe(MovePairSearch *search);		// "Подписка" на событие генерации квадрата
-	void Unsubscribe();				// Отмена подписка на событие генерации квадрата
+	Generator();					// Default constructor
+	Generator(Generator& source);	// Copy constructor
+	void Start();					// Start the squares generation
+	void Reset();					// Reset all values of internal structures
+	void SetFileNames(string start, string result, string checkpoint, string temp);	// Set names for the files of parameters and checkpoints
+	void Initialize(string start, string result, string checkpoint, string temp);	// Initialize the generator
+	void Subscribe(MovePairSearch *search);		// Subscribe to the event of square generation
+	void Unsubscribe();				            // Unsubscribe from the event of square generation
 
- 	Generator& operator = (Generator&  value);									// Оператор копирования
-	friend std::ostream& operator << (std::ostream& os, Generator& value);		// Оператор записи состояние генератора
-	friend std::istream& operator >> (std::istream& is, Generator& value);		// Оператор считывания состояния генератора
+ 	Generator& operator = (Generator&  value);									// Assignment operator
+	friend std::ostream& operator << (std::ostream& os, Generator& value);		// Operator of writing the generator state
+	friend std::istream& operator >> (std::istream& is, Generator& value);		// Operator of reading the generator state
 
 protected:
-	static const int Rank = Square::Rank;	// Ранг квадрата (для удобства)
-	static const int Free = 1;				// Флаг свободного для использования значения
-	static const int Used = 0;				// Флаг задействования значения в какой-либо клетке диагонали | строки | столбца
-	static const int Yes = 1;				// Флаг "Да"
-	static const int No = 0;				// Флаг "Нет"
-	static const int MaxCellsInPath = Rank*Rank;	// Максимальное число обрабатываемых клеток
-	int cellsInPath;				// Число обрабатываемых клеток
+	static const int Rank = Square::Rank;	// Rank (for convenience)
+	static const int Free = 1;				// Flag of the value free for usage
+	static const int Used = 0;				// Flag of the value being used in a diagonal | row | column 
+	static const int Yes = 1;				// Flag "Yes"
+	static const int No = 0;				// Flag "No"
+	static const int MaxCellsInPath = Rank*Rank;	// Maximal number of processed cells
+	int cellsInPath;				       // Number of processed cells
 
-	void CopyState(Generator& source);		// Копирование состояния
-	void Read(std::istream& is);			// Считывание состояние генератора из потока
-	void Write(std::ostream& os);			// Запись состояния генератора в поток
+	void CopyState(Generator& source);		// Copy the state
+	void Read(std::istream& is);			// Read the generator state from stream
+	void Write(std::ostream& os);			// Write the generator state into stream
 
-	Square newSquare;				// Генерируемый квадрат
+	Square newSquare;				        // Generated square
 
-	int path[MaxCellsInPath][2];			// Путь заполнения матрицы квадрата - path[i][0] - строка на шаге i, path[i][1] - столбец
-	int keyRowId;					// Идентификатор строки ключевой клетки - по значению которой расчёт будет останавливаться
-	int keyColumnId;						// Идентификатор столбца ключевой клетки
-	int keyValue;							// Значение ключевой клетки, по достижению которого расчёт будет останавливаться
+	int path[MaxCellsInPath][2];			// Path to fill the square matrix: path[i][0] is the row on step i, path[i][1] is the column
+	int keyRowId;					        // Row ID of the key cell, the value of which stops computation  
+	int keyColumnId;						// Column ID of the key cell
+	int keyValue;							// Value of the key cell which will stop computation
 
-	int primary[Rank];						// Содержимое главной диагонали
-	int secondary[Rank];					// Содержимое побочной диагонали
-	int columns[Rank][Rank];				// Матрица значений, использовавшихся в столбцах - columns[значение][столбец] = 0|1. 0 - значение занято. 1 - свободно.
-	int rows[Rank][Rank];					// Матрица значений, использовавшихся в строках - rows[строка][значение] = 0|1
-	int cellsHistory[Rank][Rank][Rank];		// Куб значений, которые использовались для формирования построенной части квадрата - cellsHistory[строка][столбец][значение]
+	int primary[Rank];						// Contents of the main diagonal
+	int secondary[Rank];					// Contents of the secondary diagonal
+	int columns[Rank][Rank];				// Matrix of the values used in columns: columns[value][column] = 0 | 1; 0 means the value has been used, 1 - it is free.
+	int rows[Rank][Rank];					// Matrix of the values used in rows: rows[row][value] = 0 | 1
+	int cellsHistory[Rank][Rank][Rank];		// Cube of the values used for generating the part of the square - cellsHistory[row][column][value]
 
-	string startParametersFileName;			// Название файла с параметрами запуска расчёта
-	string resultFileName;					// Название файла с результатами
-	string checkpointFileName;				// Название файла контрольной точки
-	string tempCheckpointFileName;			// Временное название файла новой контрольной точки
+	string startParametersFileName;			// Name of the file with start parameters for computing 
+	string resultFileName;					// Name of the file with results
+	string checkpointFileName;				// Name of the file with checkpoint
+	string tempCheckpointFileName;			// Temporary name of the file with a new checkpoint
 
-	int isInitialized;						// Флаг успешной инициализации поиска
-	int squaresCount;						// Число обнаруженных ДЛК
-	int rowId;								// Идентификатор строки обратываемой клетки
-	int columnId;							// Идентификатор столбца обрабатываемой клетки
-	int cellId;								// Идентификатор клетки в перечне шагов обхода квадрата
+	int isInitialized;						// Flag of the search initialization
+	int squaresCount;						// The number of discovered DLS
+	int rowId;								// Row ID of the processed cell
+	int columnId;							// Column ID of the processed cell
+	int cellId;								// Cell ID in the path of the square bypassing
 
 private:
-	void CreateCheckpoint();				// Создание контрольной точки
-	void ProcessSquare();					// Обработка найденного квадрата
+	void CreateCheckpoint();				// Create a checkpoint
+	void ProcessSquare();					// Process the found square
 
-	string generatorStateHeader;			// Заголовок, после которого в файле параметров или контрольной точки идёт состояние генератора диагональных квадратов
-	MovePairSearch* subscriber;			// Объект поиска ОДЛК перестановкой строк
+	string generatorStateHeader;			// Header preceding the data about search state in start parameters file or a checkpoint file
+	MovePairSearch* subscriber;			    // Object of DLS search by rows permutation
 };
 
 # endif
